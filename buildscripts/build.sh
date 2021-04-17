739c9cb85b2f7c15f673de348eca791142deeb38
Sterling Parker <caligari.87@gmail.com>
2021-04-17 08:53:20 -0600

    Update build script after some testing NOT WORKING YET

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -20,18 +20,24 @@ find \
 # MAPINFO
 rm ./mapinfo.txt
 find \
-	-name "mapinfo*"
+	-name "mapinfo*" \
 	-exec cat {} + >> ./mapinfo.txt
 
 # ZSCRIPT
 rm ./zscript.zsc
-echo "version \"4.5\"\n" >> ./zscript.zsc
-find \
-	-name "*.zsc"
-	-exec cat {} + >> ./zscript.zsc
+echo "version \"4.5\"" >> ./zscript.zsc
+for i in $(find -name "*.zsc" -not -path "*zscript.zsc" | sort -r | sed -e 's/^\.\///'); do echo "#include \"$i\"" >> ./zscript.txt ; done
+cat zscript.txt >> zscript.zsc
+rm zscript.txt
 
 # TEXTUREs
 rm ./textures.txt
 find \
-	-name "textures*"
+	-name "textures*" \
 	-exec cat {} + >> ./textures.txt
+
+# SNDINFO
+rm ./sndinfo.txt
+find \
+	-name "sndinfo*" \
+	-exec cat {} + >> ./sndinfo.txt
